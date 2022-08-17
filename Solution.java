20c427df42a54b61769a954d93a405adaa519a68
pafolder <pafolder@gmail.com>
2022-08-17 10:48:26 +0500

    Initial empty version with Hello World

---
+++ Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task39.task3908;
+
+/* 
+Возможен ли палиндром?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("I shall write the Java program to find the poliandrom here");
+    }
+
+    public static boolean isPalindromePermutation(String s) {
+return true;
+    }
+}
